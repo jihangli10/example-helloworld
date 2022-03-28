9b4112e1c4e59b517cfec4d7ebd400122944339e
Jihang Li <lijihang26@gmail.com>
2022-03-27 23:06:28 -0400

    Test

--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -26,7 +26,7 @@ pub fn process_instruction(
 ) -> ProgramResult {
     msg!("Hello World Rust program entrypoint");
 
-    // Iterating accounts is safer than indexing
+    // Iterating accounts is safer than indexinga
     let accounts_iter = &mut accounts.iter();
 
     // Get the account to say hello to
